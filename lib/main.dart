098aef5b3e098895fae9ab2dc9557e70501a9dc5
DESKTOP-AM89R6T\Administrator <1162371142@qq.com>
2021-01-11 16:24:35 +0800

    修改

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'base/BaseApp.dart';
 import 'base/router/routes.dart';
@@ -8,7 +9,15 @@ void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   BaseApp.init();
   runApp(MyApp());
+
+  // Android状态栏透明 splash为白色,所以调整状态栏文字为黑色
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.dark));
 }
+
+
 
 class MyApp extends StatelessWidget {
   @override
