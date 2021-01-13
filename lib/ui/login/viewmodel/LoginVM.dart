4d9be516d16b21a071c158fd67a122e425e34b38
DESKTOP-AM89R6T\Administrator <1162371142@qq.com>
2021-01-13 15:03:41 +0800

    修改

--- lib/ui/login/viewmodel/LoginVM.dart
+++
@@ -1,4 +0,0 @@
-import 'package:hydhome/base/provider/BaseViewModel.dart';
-
-class LoginVM extends BaseViewModel{
-}
---
+++ yipin/android/app/src/main/kotlin/com/zitian/yipinmerchant/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zitian.yipinmerchant
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
