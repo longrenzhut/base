75f4cb52dbcace55d0cc392b9844d00d67e46149
DESKTOP-AM89R6T\Administrator <1162371142@qq.com>
2021-01-15 09:12:04 +0800

    修改

--- yipin/lib/base/widget/ptr/PtrWidget.dart
+++ yipin/lib/base/widget/ptr/PtrWidget.dart
@@ -1,9 +1,58 @@
 
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:yipin/base/adapter/BaseAdapter.dart';
+import 'package:yipin/base/utils/WidgetUtils.dart';
 
 class PtrWidget extends StatelessWidget {
+
+  final PtrController controller;
+
+  final Function() onRefresh;
+
+  final VoidCallback onLoading;
+  final EdgeInsetsGeometry padding;
+  final bool shrinkWrap = false;
+  final BaseAdapter adapter;
+
+
+  const PtrWidget({Key key, this.controller, this.onRefresh, this.onLoading,
+    this.padding,
+    this.adapter}
+      ) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return SmartRefresher(
+      enablePullDown: true,
+      enablePullUp: true,
+      controller: controller,
+      onRefresh: (){
+        controller.page = 0;
+        onRefresh();
+      },
+      onLoading: (){
+        if(controller.footerStatus != LoadStatus.failed){
+          controller.page++;
+        }
+        onLoading();
+      },
+      child: WidgetUtils.buildList(
+          adapter: adapter,
+          padding:padding,
+          shrinkWrap: shrinkWrap
+      ),
+
+    );
   }
+}
+
+class PtrController extends RefreshController{
+
+  PtrController({bool initialRefresh: false}):super(initialRefresh: initialRefresh);
+
+  int page = 0;
+
+
 }
--- yipin/lib/ui/login/page/LoginPage.dart
+++ yipin/lib/ui/login/page/LoginPage.dart
@@ -3,7 +3,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lazy_load_refresh_indicator/lazy_load_refresh_indicator.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
+import 'package:yipin/base/adapter/BaseAdapter.dart';
 import 'package:yipin/base/widget/TextView.dart';
+import 'package:yipin/base/widget/ptr/PtrWidget.dart';
 import 'package:yipin/ui/login/viewmodel/LoginVM.dart';
 import '../../../base/view/BasePage.dart';
 
@@ -13,14 +15,13 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends BasePageState<LoginPage,LoginVM> {
-  
+
   @override
   LoginVM getViewModel() =>  LoginVM();
 
 
   List<String> items = List.generate(4, (index) => index.toString()).toList();
-  RefreshController _refreshController =
-  RefreshController(initialRefresh: false);
+  PtrController _refreshController = PtrController();
 
   void _onRefresh() async{
     // monitor network fetch
@@ -42,23 +43,21 @@ class _LoginPageState extends BasePageState<LoginPage,LoginVM> {
       setState(() {
 
       });
-    _refreshController.footerStatus;
     _refreshController.loadComplete();
   }
 
   @override
   Widget getView(BuildContext context) {
-    return SmartRefresher(
-        enablePullDown: true,
-        enablePullUp: true,
-        controller: _refreshController,
-        onRefresh: _onRefresh,
-        onLoading: _onLoading,
-        child: ListView.builder(
-          itemBuilder: (c, i) => Card(child: Center(child: Text(items[i]))),
-          itemExtent: 100.0,
-          itemCount: items.length,
-        ),
+    return PtrWidget(
+      controller: _refreshController,
+      onRefresh: _onRefresh,
+      onLoading: _onLoading,
+      adapter: BaseAdapter<String>(
+          data: items,
+          builder: (context,index,model){
+            return Card(child: Center(child: Text(model)));
+          }
+      ),
 
     );
   }
