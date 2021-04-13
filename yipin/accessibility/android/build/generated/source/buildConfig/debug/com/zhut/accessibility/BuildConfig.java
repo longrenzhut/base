c954510747a96d6ee4d9468980180366ea508c78
DESKTOP-AM89R6T\Administrator <1162371142@qq.com>
2021-04-13 08:45:33 +0800

    1111

---
+++ yipin/accessibility/android/build/generated/source/buildConfig/debug/com/zhut/accessibility/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.zhut.accessibility;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.zhut.accessibility";
+  public static final String BUILD_TYPE = "debug";
+}
